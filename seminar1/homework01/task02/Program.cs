77f53efd72af50ff8f04cbaee9827f725e7f04b8
NataliyaSavva <n_savva@list.ru>
2022-10-07 18:55:29 +0300

    первая задача домашнего задания

---
+++ seminar1/homework01/task02/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("1 вариант");
+int x = 5, y = 7;
+int max = x;
+if (max < y)
+{
+    max =y;
+}
+Console.WriteLine("Максимальное число " + max);
+Console.WriteLine("end ");
+Console.WriteLine("2 вариант");
+x = 2;
+y = 10;
+max = x;
+if (max < y)
+{
+    max =y;
+}
+Console.WriteLine("Максимальное число " + max);
+Console.WriteLine("end ");
+Console.WriteLine("3 вариант");
+x = -9;
+y = -3;
+max = x;
+if (max < y)
+{
+    max =y;
+}
+Console.WriteLine("Максимальное число " + max);
+Console.WriteLine("end ");
+Console.WriteLine("4 вариант");
+Console.WriteLine("Введите первое целое число: ");
+x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+y = int.Parse(Console.ReadLine());
+max = x;
+if (max < y)
+{
+    max =y;
+}
+Console.WriteLine("Максимальное число " + max);
