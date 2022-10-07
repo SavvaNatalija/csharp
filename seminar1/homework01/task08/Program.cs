a1b6d326334ff2d5a2548a4584ece1132a0b57e6
NataliyaSavva <n_savva@list.ru>
2022-10-07 21:24:00 +0300

    четвертая задача домашнего задания

---
+++ seminar1/homework01/task08/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("start");
+Console.WriteLine("Введите целое положительное число");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    Console.Write(i + " ");
+    i+=2;
+}
+Console.WriteLine("end");
