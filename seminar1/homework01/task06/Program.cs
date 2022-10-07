6f02800537676908108ff9d8bda950f098c45fae
NataliyaSavva <n_savva@list.ru>
2022-10-07 20:47:33 +0300

    третья задача домашнего задания

---
+++ seminar1/homework01/task06/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("1 вариант");
+int x = 4;
+string answer = "четное число";
+if ((x%2) > 0)
+{
+    answer = "число нечетное";
+}
+Console.WriteLine(x + " - " + answer);
+Console.WriteLine("2 вариант");
+x = -3;
+answer = "четное число";
+if ((x%2) > 0)
+{
+    answer = "число нечетное";
+}
+Console.WriteLine(x + " - " + answer);
+Console.WriteLine("3 вариант");
+x = 7;
+answer = "четное число";
+if ((x%2) > 0)
+{
+    answer = "число нечетное";
+}
+Console.WriteLine(x + " - " + answer);
+Console.WriteLine("4 вариант, введите целое число");
+x = int.Parse(Console.ReadLine());
+answer = "четное число";
+if ((x%2) > 0)
+{
+    answer = "число нечетное";
+}
+Console.WriteLine(x + " - " + answer);
