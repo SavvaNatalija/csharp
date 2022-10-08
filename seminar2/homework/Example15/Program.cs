cd0b1fd052760d6e95e657d63e1a162b7c46b14a
NataliyaSavva <n_savva@list.ru>
2022-10-08 12:40:59 +0300

    добавлена третья задача домашнего задания

---
+++ seminar2/homework/Example15/Program.cs
@@ -0,0 +1,19 @@
+﻿void Verification(int a)
+{
+    if ((a>0)&(a<6))
+    {
+        Console.WriteLine($"{a} - это рабочий день");
+    }
+    else if ((a==6)|(a==7))
+    {
+        Console.WriteLine($"{a} - это выходной день");
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число");
+    }
+}
+
+Console.WriteLine("Введите цифру дня недели: ");
+int x = int.Parse(Console.ReadLine());
+Verification(x);
