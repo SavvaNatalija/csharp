b1f05f424c674bd489894558341805c325fb6fa4
NataliyaSavva <n_savva@list.ru>
2022-10-08 12:25:30 +0300

    вторая задача домашнего задания

---
+++ seminar2/homework/Example13/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int ThirdDigit (int a)
+{
+    int result = ((a / 10) / 10) % 10;
+    return result;
+}
+
+int n = 12321;
+int value = n;
+
+Console.WriteLine("Введите трехзначное целое число: ");
+
+int x = int.Parse(Console.ReadLine());
+
+int b = ThirdDigit(x);
+
+if (b > 0)
+{
+Console.WriteLine("Третья цифра данного числа = " + b);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
