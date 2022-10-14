d19f478eaaae6bc7606050699a134d3fe6361433
NataliyaSavva <n_savva@list.ru>
2022-10-15 00:04:38 +0300

    решена третья задача домашней работы

---
+++ seminar3/homework/example23/Program.cs
@@ -0,0 +1,23 @@
+﻿string Cube(int a)
+{
+    int i = 1;
+    string cube = String.Empty;
+    while (i <= a)
+    {
+        cube = cube + ' ' + i * i * i;
+        i++;
+    }
+    return cube;
+}
+
+int t = -1;
+bool flag = false;
+
+while (!flag)
+{
+    Console.Write("Введите число ");
+    string data = Console.ReadLine();
+    flag = int.TryParse(data, out t);
+}
+
+Console.WriteLine(Cube(t));
