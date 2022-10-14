d51bfb775ecd257d347c8f4106b3eb93a2a729f1
NataliyaSavva <n_savva@list.ru>
2022-10-14 23:35:47 +0300

    решена первая задача домашней работы

---
+++ seminar3/example22/Program.cs
@@ -0,0 +1,72 @@
+﻿// 5 ->1, 4, 9, 16, 25
+
+int n = 5;
+
+GetSquare1(n);
+
+void GetSquare1(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        System.Console.Write((i * i) + " ");
+        i++;
+    }
+}
+
+int[] GetSquare2(int n)
+{
+    int i =0;
+    int[] squares = new int[n];
+    while (i<n)
+    {
+        squares[i] = ((i + 1) * (i + 1));
+        i++;
+    }
+    return squares;
+}
+
+System.Console.WriteLine(String.Join(' ', GetSquare2(n)));
+
+//Console.WriteLine(GetSquare2(n));
+
+string GetSquare3(int n)
+{
+    int i = 1;
+    string res = String.Empty;
+    while (i<=n)
+    {
+        res += $"{Convert.ToString(i * i) } ";
+        //res = Convert.ToString(i * i) + ' ' + res;
+        i++;
+    }
+    return res;
+}
+
+System.Console.WriteLine(GetSquare3(6));
+
+int v;
+string data = "132";
+
+bool flag = int.TryParse(data, out v); //проверка значение, численное ли?
+if (flag)
+{
+    System.Console.WriteLine(v);    
+}
+else
+{
+    System.Console.WriteLine("Данные кривые");
+}
+
+int t = -1;
+// добавим условие от 11 до 99
+flag = false;
+
+while (!(flag && t > 10 && t < 100))
+{
+    Console.Write("Введите число ");
+    string dat = Console.ReadLine();
+    flag = int.TryParse(dat, out t);
+}
+
+System.Console.WriteLine(t);
---
+++ seminar3/homework/example19/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int FlipNumber (int v)
+{
+    int r = v / 10;
+    int a = v % 10;
+    while (r > 0)    
+    {
+        a = a * 10  + r % 10; 
+        r = r / 10;
+    }
+    return a;
+}
+
+
+
+int t = -1;
+bool flag = false;
+
+while (!flag)
+{
+    Console.Write("Введите число ");
+    string data = Console.ReadLine();
+    flag = int.TryParse(data, out t);
+}
+
+int z = FlipNumber(t);
+if (t == z)
+{
+    Console.WriteLine($"число {t} - палиндром");
+}
+else
+{
+    Console.WriteLine($"число {t} - не является палиндромом");
+}
