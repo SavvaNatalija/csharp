a6f561d7f955953ee1f75b56a5a693e97386a20e
NataliyaSavva <n_savva@list.ru>
2022-10-30 12:57:03 +0300

    добавлено домашнее задание №8

---
+++ homework8/example54/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void PrintArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10); //[1,10)
+        }
+    }
+}
+
+void SortArray (int[,] matr)
+{
+    int temp;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        temp = 0;       
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = j+1; k < matr.GetLength(1); k++)
+            {
+                if (matr[i, j] < matr[i, k])
+                {
+                    temp = matr[i, j];
+                    matr[i, j] = matr[i, k];
+                    matr[i, k] = temp;
+                }                
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[4, 4];
+
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
+SortArray(matrix);
+PrintArray(matrix);
---
+++ homework8/example56/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+void PrintArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10); //[1,10)
+        }
+    }
+}
+
+void SearchMinLine (int[,] matr)
+{
+    int[] matr2 = new int[matr.GetLength(0)];    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        matr2[i] = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr2[i] += matr[i,j];
+        }
+    }
+    int min = matr2[0];
+    int number = 0; 
+    for (int i = 0; i < matr2.GetLength(0); i++)
+    {
+        if (min > matr2[i]) 
+        {
+            min = matr2[i];
+            number = i;
+        }
+    }
+    Console.WriteLine($"Номер строки, с наименьшей суммой элементов ({min}) - {number}");
+}
+
+int[,] matrix = new int[4, 4];
+
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
+SearchMinLine(matrix);
